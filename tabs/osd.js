b1c410b2bd1cebaee5c590aa298fe9a32604651c
nathan <nathan@vertile.com>
2016-09-22 23:37:33 -0700

    fix the upgrade path, dont require a configurator restart to get the new fields

--- tabs/osd.js
+++ tabs/osd.js
@@ -586,9 +586,7 @@ TABS.osd.initialize = function (callback) {
           // ask for the OSD config data
           MSP.promise(MSPCodes.MSP_OSD_CONFIG)
           .then(function(info) {
-            if (!('DISPLAY_FIELDS' in OSD.constants)) {
-                OSD.chooseFields();
-            }
+            OSD.chooseFields();
             if (!info.length) {
               $('.unsupported').fadeIn();
               return;
